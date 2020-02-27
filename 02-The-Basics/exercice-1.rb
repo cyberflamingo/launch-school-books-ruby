49660b4c6500245386d2be54ff2f97618115b3f7
CyberFlamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-02-27 18:40:23 +0900

    Chapter 2 Exercies 1 and 2

---
+++ 02-The-Basics/exercice-1.rb
@@ -0,0 +1,11 @@
+firstname = "Cyber"
+lastname = "Flamingo"
+
+no_space = firstname + lastname
+with_space = firstname + " " + lastname
+
+puts no_space
+puts with_space
+
+puts "With interpolation, no space: #{firstname}#{lastname}"
+puts "With interpolation, with space: #{firstname} #{lastname}"
