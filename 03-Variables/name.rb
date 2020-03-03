16f79add71f498b2f8ea81c73b1c697f4812f594
CyberFlamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-03-03 13:41:15 +0900

    Chapter 3 Exercice 4

--- 03-Variables/name.rb
+++ 03-Variables/name.rb
@@ -1,7 +1,14 @@
-puts "What's your name?"
-name = gets.chomp
+puts "What's your first name?"
+first_name = gets.chomp
 
-puts "Welcome to ruby, #{name}!"
+puts "What's your last name?"
+last_name = gets.chomp
+
+puts "Welcome to ruby, #{first_name} #{last_name}!"
+
+puts ""
+
+name = first_name
 
 10.times do puts name end
 
