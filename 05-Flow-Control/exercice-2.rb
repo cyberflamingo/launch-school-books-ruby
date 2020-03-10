6c47b921248b60e4b50b90f7a23384ba89c3061c
CyberFlamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-03-10 14:13:31 +0900

    Fix second proposition

--- 05-Flow-Control/exercice-2.rb
+++ 05-Flow-Control/exercice-2.rb
@@ -1,10 +1,10 @@
 def yell(string)
-  return string.upcase! unless (string.length < 10)
-  #if string.length > 10 then return string.capitalize end
+  return string.upcase! unless (string.length <= 10)
+  #if string.length > 10 then return string.upcase! end
 end
 
 str1 = "aaaaaaaaa"
-str2 = "aaaaaaaaaa"
+str2 = "aaaaaaaaaaa"
 
 puts "str1: " + yell(str1).to_s
 puts "str2: " + yell(str2).to_s
