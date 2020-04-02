e58ab02c5f0011c12a55fc4c49b004af857b96da
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-04-02 22:52:53 +0900

    Chapter 7 Exercice 5

---
+++ 07_Arrays/exercice_5.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+string = 'Welcome to America!'
+puts a = string[6] # e
+puts b = string[11] # A
+puts c = string[19] # ~~error out of range~~
+# Well nope, it was \n new line
