d34a51ed0adf29abc04b517aad87c7f7c52700a9
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-04-06 22:25:21 +0900

    Chapter 9 Exercice 3

---
+++ 09_more_stuff/exercice_3.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+puts 'Exception handling describes the action of code falling back to a '    \
+  'special bit of codes when encountering an error. This is usefull because' \
+  'the code won\'t stop at the error.'
