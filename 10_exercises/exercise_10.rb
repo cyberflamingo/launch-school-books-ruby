9e7412c84756cb4564dce13d4ad79a90f4824a57
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-04-12 15:03:46 +0900

    Chapter 10 Exercise 10

---
+++ 10_exercises/exercise_10.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+puts 'Hash values can be whatever we want:'
+
+puts hash = { 'array' => [1, 2, 3] }
+
+puts 'You can also have an array of hashes:'
+
+puts array = [{ hash: 1, hash2: 2 }]
